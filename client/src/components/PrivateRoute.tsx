40d70c908f869e114aa4b1325e7e4c6675ee706d
oscarigo <59118913+Os-humble-man@users.noreply.github.com>
2025-03-20 22:53:01 +0200

    add privateRoute file and redirection file

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,33 +1,16 @@
+import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useAuthStore } from "@/store/authStore";
-import { useEffect, useState } from "react";
 
-const PrivateRoute = ({ requiredRole }: { requiredRole?: string }) => {
-  const { isAuthenticated, user, checkAuth } = useAuthStore();
-  const [isLoading, setIsLoading] = useState(true);
+const ProtectedRoute: React.FC = () => {
   const location = useLocation();
 
-  useEffect(() => {
-    const verifyAuth = async () => {
-      await checkAuth();
-      setIsLoading(false);
-    };
-    verifyAuth();
-  }, [checkAuth]);
-
-  if (isLoading) {
-    return <p>Chargement...</p>;
-  }
+  const isAuthenticated = !!sessionStorage.getItem("accessToken");
 
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  if (requiredRole && user?.role !== requiredRole) {
-    return <Navigate to="/unauthorized" replace />;
-  }
-
   return <Outlet />;
 };
 
-export default PrivateRoute;
+export default ProtectedRoute;
